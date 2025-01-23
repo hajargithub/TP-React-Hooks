e5a028fe1faab73e49e3aa016dfe07fb4ff9d108
HARKAOUI Hajar <hajar15harkaoui@gmail.com>
2025-01-23 11:58:02 +0100

    AdduseDebounce_Exo1.2

--- src/components/ProductSearch.js
+++ src/components/ProductSearch.js
@@ -1,5 +1,6 @@
 import React, { useState, useContext } from 'react';
 import { ThemeContext } from '../App';
+import useDebounce from '../hooks/useDebounce';
 
 const ProductSearch = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -7,7 +8,7 @@ const ProductSearch = () => {
   // TODO: Exercice 2.1 - Utiliser le LanguageContext
   
   // TODO: Exercice 1.2 - Utiliser le hook useDebounce
-  
+    const debouncedSearchTerm = useDebounce(searchTerm, 500); // Debounce de 500 ms
   return (
     <div className="mb-4">
       <input
